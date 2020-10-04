acc2d6fff170071903860304bbbdf6db1834877b
Ravi <miravishankar@yahoo.co.in>
2020-10-04 04:20:57 +0200

    Fuzzing with LLVM, AFL, AFL++

--- Fuzzing/Practice/mxml/fuzzer.c
+++ Fuzzing/Practice/mxml/fuzzer.c
@@ -1,19 +1,14 @@
 #include<stdint.h>
 #include "mxml.h"
 
-int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size){
-    if(Size == 0  ){
+int main(int argc, char *argv[]){
+    if(argc != 2  ){
         return 0;
     }
 
-    if(Size % 4 !=0 ){
-        return 0;
-    }
-
-    if(Data[Size-1] != '\x00'){
-        return 0;
-    }
-    mxml_node_t *top = mxmlLoadString(NULL, Data, MXML_OPAQUE_CALLBACK);
+    FILE *fp =  fopen(argv[1], 'r');
+    mxml_node_t *top = mxmlLoadString(NULL, fp, MXML_OPAQUE_CALLBACK);
+    mxmlDelete(top);
     return 0;
     
 }
